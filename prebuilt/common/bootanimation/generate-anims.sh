1814c15a56180e759a801efe2ae143c8b2492ff6
Jacob McSwain <jacob.a.mcswain@gmail.com>
2017-09-15 02:51:13 -0500

    New Bootanimations

--- prebuilt/common/bootanimation/generate-anims.sh
+++ prebuilt/common/bootanimation/generate-anims.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-RESOLUTIONS="240 320 360 480 540 600 720 768 800 1080 1200 1440"
+RESOLUTIONS="240 320 360 480 540 600 720 768 800 1080 1200"
 
 for i in $RESOLUTIONS; do
 	rm -f $i.zip
 	mkdir $i
 	cd $i
-	unzip ../bootanimation.zip
+	unzip ../1440.zip
 	for j in */*.[pP][nN][gG]; do
-		convert $j -resize "$i"x$(bc <<< "scale=0;$i*0.4186/1") tmp.png
+		convert $j -resize "$i"x$(bc <<< "scale=0;$i*0.6583/1") tmp.png
 		pngquant --force --output $j tmp.png
 		rm tmp.png
 	done
 	frameliney=$i
-	framelinex=$(bc <<< "scale=0; $i*0.4186/1")
+	framelinex=$(bc <<< "scale=0; $i*0.6583/1")
 	frameline=$(echo $frameliney $framelinex 30)
 	sed "1s/.*/$frameline/" desc.txt > desc.tmp
 	mv desc.tmp desc.txt
